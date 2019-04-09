2a12be39c131f535225745c14ed166d34cbba0fc
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-04-10 00:06:52 +0600

    Add files via upload

---
+++ 2=navbar.php
@@ -0,0 +1,89 @@
+<?php
+
+   <style>
+   .navbar{
+	        border-bottom: 3px solid #008ed6;
+	        background: #292b2c;
+	        opacity: 0.7;
+        }
+   </style>
+
+/*just-lakha jonno*/
+  <nav class="navbar navbar-dark navbar-expand-md fixed-top">
+    <div class="container">
+      <a href="#home-section" class="navbar-brand">LoopLAB</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#home-section">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#explore-head-section">Explore</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#create-head-section">Create</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#share-head-section">Share</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  /*logo and lakha*/
+
+<style>
+.navbar {
+  box-shadow: 2px 2px 5px #3292a6;
+  background: #fff;
+  opacity: 0.9; 
+  }
+  .navbar .nav-item {
+    font-size: 1.4rem;
+    padding-right: 20px; 
+   }
+    .navbar .nav-item .nav-link {
+      color: #252525; 
+   }
+    .navbar .nav-item .nav-link:hover {
+      color: #3292a6;
+   }
+  .navbar .nav-item:last-child {
+    padding-right: 0; 
+  }
+</style>
+
+-------
+------------>
+    <nav class="navbar navbar-light navbar-expand-lg py-3 fixed-top">
+    <div class="container">
+      <a href="#showcase" class="navbar-brand">
+        <img src="img/mlogo.png" class="img-fluid" width="50" height="50" alt="M">
+        <h3 class="d-inline align-middle">Mizuxe</h3>
+      </a>
+      <button class="navbar-toggler navbar-toggler-right" data-toggle="collapse" data-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#showcase">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#authors">Meet The Authors</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+?>
