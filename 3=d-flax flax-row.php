7b9b5b3ae344fc8b972088f1157fb59c88ef8e14
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-04-10 00:07:09 +0600

    Add files via upload

---
+++ 3=d-flax flax-row.php
@@ -0,0 +1,47 @@
+<?
+
+
+<style>
+#home-section {
+  background: url("../img/home.jpg") no-repeat center top;
+  background-size: cover;
+  background-attachment: fixed;
+  min-height: 700px; 
+}
+
+ #home-section .dark-overly{
+    background: rgba(0, 0, 0, 0.7);
+    min-height: 700px;
+
+} 
+#home-section .home-inner{
+    padding-top: 150px;
+   padding-bottom: 50px;
+}
+
+#home-section .fa{
+	background: #fff;
+	font-size: 30px;
+	color: #008ed6;
+	padding: 4px;
+	border-radius: 5px;
+}
+ #home-section .card-form {
+    opacity: 0.8;
+     }
+ 
+</style>
+
+<div class="col-lg-8 col-md-12 d-none d-sm-block">
+    <h1 class="display-4">Build social profiles and gain revenue and profits</h1>
+        <div class="d-flex flex-row">
+            <div class="p-4 align-self-start mt-2">
+                    <i class="fa fa-check"></i>
+            </div>
+            <div class="p-4 align-self-end">
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas officiis iste itaque et vel commodi nulla voluptate cupiditate debitis laborum?</p>
+            </div>
+        </div>
+</div>
+
+?>
