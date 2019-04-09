80b587af42a10b2e2585a69f09ebd4847bc4061c
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-04-10 00:08:26 +0600

    Add files via upload

---
+++ 7=Acordium.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+
+
+
+    <div id="accordion" class="pt-5" role="tablist">
+            <div class="card">
+              <div class="card-header" role="tab">
+                <h5 class="mb-0">
+                  <div data-toggle="collapse" href="#collapse1">
+                    <i class="fa fa-arrow-circle-down"></i> Get Inspired
+                  </div>
+                </h5>
+              </div>
+              <div id="collapse1" class="collapse show" data-parent="#accordion">
+                <div class="card-body">
+                  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consectetur accusantium, molestias quos, distinctio, labore assumenda repellendus, beatae adipisci fuga explicabo iure quia. Facilis ratione, aut, impedit id rerum voluptate?</p>
+                </div>
+              </div>
+            </div>
+            <div class="card" role="tablist">
+              <div class="card-header">
+                <h5 class="mb-0">
+                  <div data-toggle="collapse" href="#collapse2">
+                    <i class="fa fa-arrow-circle-down"></i> Gain The Knowledge
+                  </div>
+                </h5>
+              </div>
+              <div id="collapse2" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consectetur accusantium, molestias quos, distinctio, labore assumenda repellendus, beatae adipisci fuga explicabo iure quia. Facilis ratione, aut, impedit id rerum voluptate?</p>
+                </div>
+              </div>
+            </div>
+            <div class="card" role="tablist">
+              <div class="card-header">
+                <h5 class="mb-0">
+                  <div data-toggle="collapse" href="#collapse3">
+                    <i class="fa fa-arrow-circle-down"></i> Open Your Mind
+                  </div>
+                </h5>
+              </div>
+              <div id="collapse3" class="collapse" data-parent="#accordion">
+                <div class="card-body">
+                  <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consectetur accusantium, molestias quos, distinctio, labore assumenda repellendus, beatae adipisci fuga explicabo iure quia. Facilis ratione, aut, impedit id rerum voluptate?</p>
+                </div>
+              </div>
+            </div>
+    </div>
+
+?>
