4d0d1ebb671f1886defac7ad783ab7eb3c028e63
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-04-10 12:51:49 +0600

    Add files via upload

---
+++ 12.Gallery.php
@@ -0,0 +1,67 @@
+
+//Add uikit.min.css
+//Add uikit.min.js
+//Add uikit.icons.min.js
+
+<style type="text/css">
+	#gallery img {
+    min-height: 250px; 
+  }
+</style>
+<!-- Photo Gallery -->
+  <section id="gallery" class="py-5 text-center" uk-lightbox>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Photo Gallery</h2>
+          <p class="lead">Check out our photos</p>
+        </div>
+      </div>
+      <div class="row py-0 py-sm-3">
+        <div class="col-md-4">
+          <div>
+            <a href="img/image1.jpeg">
+              <img src="img/image1.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div>
+            <a href="img/bg1.jpeg">
+              <img src="img/bg1.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div>
+            <a href="img/image3.jpeg">
+              <img src="img/image3.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row py-0 py-sm-2">
+        <div class="col-md-4">
+          <div>
+            <a href="img/image4.jpeg">
+              <img src="img/image4.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div>
+            <a href="img/image5.jpeg">
+              <img src="img/image5.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div>
+            <a href="img/image6.jpeg">
+              <img src="img/image6.jpeg" class="img-fluid" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
