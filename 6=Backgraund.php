2c8cf63d1bb4f8a0dea6adb1a13baf7960ac59c9
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-09-14 03:39:57 -0700

    Update 6=Backgraund.php

--- 6=Backgraund.php
+++ 6=Backgraund.php
@@ -2,9 +2,28 @@
 
 
 
-    background-image: url("../img/mountains.jpg") no-repeat center top;
+
+    background:linear-gradient(rgba(135, 80, 156, 0.9),rgba(135, 80, 156, 0.9)),url('img/hero-bg.jpg');
+    background-repeat: no-repeat;
+    background-position:  center (center/top/bottom);
     background-size: cover;
     background-attachment: fixed;
     min-height: 600px;
+
+  ###  Or-----Short form-----***
+
+
+    background-image: url("../img/mountains.jpg") no-repeat fixed center top;
+    background-size: cover;
+    min-height: 600px;
+
+
+
+
+   *** Overlay + Background ***
+    background:linear-gradient(rgba(135, 80, 156, 0.9),rgba(135, 80, 156, 0.9)),url(img/hero-bg.jpg);
+
+  *** Color + Background ***
+     background: lightblue url("../img/mountains.jpg") no-repeat fixed center;
 
 ?>
